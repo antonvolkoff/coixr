c2b0c4622f0fb6a2c3ab9cad37e728fd9afda2b8
Anton Volkov <choixer@gmail.com>
2014-03-20 12:20:54 +0100

    Style for search results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,13 @@
 class SearchesController < ApplicationController
+  Result = Struct.new(:title, :author)
+
   def index 
   end
 
   def create
+    @results = []
+    10.times do |n|
+      @results << Result.new("Mega article ##{n}")
+    end
   end
 end

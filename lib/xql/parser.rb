04a30a0a9a1590d46ec779af02ef93b7502310c4
Anton Volkov <choixer@gmail.com>
2014-03-21 20:42:43 +0100

    Add value, key and condition rulles

--- lib/xql/parser.rb
+++ lib/xql/parser.rb
@@ -8,6 +8,25 @@ class XQL::Parser < Parslet::Parser
   # rule(:query) { category >> (space >> where >> space >> (condition).repeat.as(:conditions)).maybe }
   # root :query
 
-  rule(:string) { str('"') >> (str('"').absent? >> any).repeat >> str('"') }
-  rule(:value)  { string }
+  rule(:space)      { match["\t "] }
+  rule(:whitespace) { space.repeat }
+
+  rule(:digit) { match('[0-9]') }
+
+  rule(:integer)  { str('-').maybe >> match("[1-9]") >> digit.repeat }
+  rule(:float)    { str('-').maybe >> digit.repeat(1) >> str('.') >> digit.repeat(1) }
+  rule(:boolean)  { str('true') | str('false') }
+  rule(:string)   { str('"') >> (str('"').absent? >> any).repeat >> str('"') }
+  
+  rule(:value) do 
+    integer | float | boolean | string
+  end
+
+  rule(:key) do 
+    (match["\\[\\]=."].absent? >> space.absent? >> any).repeat(1)
+  end
+
+  rule(:condition) do
+    whitespace >> key >> whitespace >> str('=') >> whitespace >> value  
+  end
 end
--- spec/lib/xql/parser_spec.rb
+++ spec/lib/xql/parser_spec.rb
@@ -18,6 +18,7 @@ describe XQL::Parser do
       expect(subject).to      parse("0.1")
       expect(subject).to      parse("3.14159")
       expect(subject).to      parse("-0.00001")
+      expect(subject).to      parse("12.3456")
       expect(subject).to_not  parse(".1")
     end
 
@@ -31,5 +32,24 @@ describe XQL::Parser do
       expect(subject).to      parse('""')
       expect(subject).to      parse('"hello world"')
     end
+  end
+
+  describe 'key' do
+    let(:rule) { :key }
+
+    it { expect(subject).to     parse('foobar') }
+    it { expect(subject).not_to parse("lolwhat.noWAY") }
+    it { expect(subject).not_to parse('no whitespace') }
+    it { expect(subject).not_to parse('noequal=thing') }
+  end
+
+  describe 'condition' do
+    let(:rule) { :condition }
+
+    it { expect(subject).to parse('key=3.14') }
+    it { expect(subject).to parse('key = 10') }
+    it { expect(subject).to parse('key = 10.12') }
+    it { expect(subject).to parse('key = true') }
+    it { expect(subject).to parse('key = "value"') }
   end
 end

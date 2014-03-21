378db5369477361e3b1ef33fd880d27d868a0271
Anton Volkov <choixer@gmail.com>
2014-03-21 18:49:25 +0100

    Parse strings values and add tests for integers, floats and boolean

--- lib/xql/parser.rb
+++ lib/xql/parser.rb
@@ -1,10 +1,13 @@
 class XQL::Parser < Parslet::Parser
-  rule(:space)      { match('\s').repeat(1) }
-  rule(:space?)     { space.maybe }
-  rule(:category)   { str("articles").as(:category) }
-  rule(:where)      { str("where") }
-  rule(:condition)  { (str("author = \"John\"") >> space?) }
+  # rule(:space)      { match('\s').repeat(1) }
+  # rule(:space?)     { space.maybe }
+  # rule(:category)   { str("articles").as(:category) }
+  # rule(:where)      { str("where") }
+  # rule(:condition)  { (str("author = \"John\"") >> space?) }
 
-  rule(:query) { category >> (space >> where >> space >> (condition).repeat.as(:conditions)).maybe }
-  root :query
+  # rule(:query) { category >> (space >> where >> space >> (condition).repeat.as(:conditions)).maybe }
+  # root :query
+
+  rule(:string) { str('"') >> (str('"').absent? >> any).repeat >> str('"') }
+  rule(:value)  { string }
 end
--- spec/lib/xql/parser_spec.rb
+++ spec/lib/xql/parser_spec.rb
@@ -1,18 +1,35 @@
 require 'spec_helper'
 
 describe XQL::Parser do
-  subject { XQL::Parser.new.parse(xql) } 
+  subject { XQL::Parser.new.send(rule) }
 
-  context 'when <category>' do
-    let(:xql) { "articles" }
-    let(:result) { {category: "articles"} }
-    it { expect(subject).to eq(result) }
-  end
+  describe 'value' do
+    let(:rule) { :value }
 
-  context 'when <category where condition>' do
-    let(:xql) { "articles where author = \"John\"" }
-    let(:result) { {category: "articles", conditions: "author = \"John\""} }
+    it "parses integers" do
+      expect(subject).to      parse("1")
+      expect(subject).to      parse("-123")
+      expect(subject).to      parse("120381")
+      expect(subject).to      parse("181")
+      expect(subject).to_not  parse("0181")
+    end
 
-    it { expect(subject).to eq(result) }
+    it "parses floats" do
+      expect(subject).to      parse("0.1")
+      expect(subject).to      parse("3.14159")
+      expect(subject).to      parse("-0.00001")
+      expect(subject).to_not  parse(".1")
+    end
+
+    it "parses booleans" do
+      expect(subject).to      parse("true")
+      expect(subject).to      parse("false")
+      expect(subject).to_not  parse("truefalse")
+    end
+
+    it 'parses strings' do
+      expect(subject).to      parse('""')
+      expect(subject).to      parse('"hello world"')
+    end
   end
 end

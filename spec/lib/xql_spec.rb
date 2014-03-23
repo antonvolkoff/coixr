5b8442c085be77d35f0e56f418b5923a6d7a5ba2
Anton Volkov <choixer@gmail.com>
2014-03-23 21:16:53 +0100

    Add parses to searches controller

--- spec/lib/xql_spec.rb
+++ spec/lib/xql_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 describe XQL do
   subject { XQL.parse(query) }
 
-  context 'when full query is given' do
+  context 'when query with one condition' do
     let(:query) { 'articles where author = "John"' }
 
     it { expect(subject).to eq({_type: "Article", author: "John"}) }
+  end
+
+  context 'when simple query' do
+    let(:query) { 'articles' }
+
+    it { expect(subject).to eq({_type: "Article"}) }
   end
 end

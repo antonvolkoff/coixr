2d072f8609134f920d55be4351693bb091c1580e
Anton Volkov <choixer@gmail.com>
2014-03-23 15:11:13 +0100

    Add specs for interactor SearchWithConditions

--- spec/interactors/search_with_conditions_spec.rb
+++ spec/interactors/search_with_conditions_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe SearchWithConditions do
-  pending
+  let(:node_class) { double('Node') }
+  let(:conditions) { {_type: 'Test'} }
+  subject do 
+    SearchWithConditions.perform(conditions: conditions, node_class: node_class)
+  end
+  before do 
+    allow(node_class).to receive(:where).with(conditions).and_return(nodes)
+  end
+
+  context 'when seach is successful' do
+    let(:nodes) { ['node'] }
+
+    it { expect(subject.success?).to eq(true) }
+    it { expect(subject.nodes).to eq(['node']) }
+  end
+
+  context 'when search is failed' do
+    let(:nodes) { [] }
+
+    it { expect(subject.success?).to eq(false) }
+    it { expect(subject.nodes).to eq([]) }
+  end
 end

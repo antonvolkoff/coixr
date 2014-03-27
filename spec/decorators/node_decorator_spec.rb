3b1f225d6d10b0eed5893877809472cebd7829da
Anton Volkov <choixer@gmail.com>
2014-03-27 14:20:37 +0100

    Add spec for NodeDecorator

--- spec/decorators/node_decorator_spec.rb
+++ spec/decorators/node_decorator_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
 describe NodeDecorator do
+  let(:node) { double('node') }
+  subject { NodeDecorator.new(node).send(method_name) }
+
+  describe '.link' do
+    let(:method_name) { :link }
+    before do
+      allow(node).to receive(:title).and_return('Title')
+      allow(node).to receive(:url).and_return('http://example.com')
+    end
+
+    it { expect(subject).to eq("<a href=\"http://example.com\">Title</a>") }
+  end
+
+  describe '.host' do
+    let(:method_name) { :host }
+    before do
+      allow(node).to receive(:url).and_return('http://example.com/article/1')
+    end
+
+    it { expect(subject).to eq('http://example.com') }
+  end
 end
